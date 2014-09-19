59361a1e6669c25b7ca0a12fdebc7482b6dfb2ed
Allan Leon <allan.leon@outlook.com>
2014-09-18 22:08:09 -0400

    Updated Point class

--- src/bezier/model/basics/Point.java
+++ src/bezier/model/basics/Point.java
@@ -1,27 +1,28 @@
 package bezier.model.basics;
 
-public class Point extends Coordinates {
+public class Point {
 
-	public Point(double x, double y, double z) {
-		super(x, y, z);
-		w = 1;
-	}
+	protected double x;
+	protected double y;
 	
-	public void checkW() {
-		if (w == 0) {
-			w = 1;
-		} else if (w != 1) {
-			x /= w;
-			y /= w;
-			z /= w;
-			w /= w;
-		}
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
 	}
-	
-	public double getDistanceTo(Point p) {
-		double dx = x - p.getX();
-		double dy = y - p.getY();
-		double dz = z - p.getZ();
-		return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2) + Math.pow(dz, 2));
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
 	}
 }
